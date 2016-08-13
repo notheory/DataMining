2ddddd95a082c5d2fb00c78210d0f8a39e5b337f
Zhongjie Fu <453425452@qq.com>
2016-08-13 11:08:17 +0800

    try plot movie boxoffice

--- movie.py
+++ movie.py
@@ -1,5 +1,12 @@
+# -*- coding=utf-8 -*-
 import tushare as ts
+import matplotlib.pyplot as plt
 # to analyse movie data
 
-movie_df = ts.realtime_boxoffice()
-print movie_df
+while True:
+    movie_df = ts.realtime_boxoffice()
+    shituxingzhe_df = movie_df[movie_df['MovieName']==u'使徒行者']
+    weiweiyixiao_df = movie_df[movie_df['MovieName']==u'微微一笑很倾城']
+    daomubiji_df = movie_df[movie_df['MovieName']==u'盗墓笔记']
+    plt.plot(shituxingzhe_df.at[0, 'BoxOffice'])
+    print movie_df
